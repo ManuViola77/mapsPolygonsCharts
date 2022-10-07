1ab7c3662e534ec732fd15b91a0f754aa17f2e1e
Manuela Viola <manuela.viola@rootstrap.com>
2022-10-07 19:45:48 +0200

    feat: add map with polygon

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+import Map from './src/screens/Map';
+
+const App = () => (
+  <SafeAreaView>
+    <Map />
+  </SafeAreaView>
+);
+
+export default App;
---
+++ src/constants/common.js
@@ -0,0 +1,6 @@
+import {Dimensions} from 'react-native';
+
+const {height, width} = Dimensions.get('window');
+
+export const SCREEN_HEIGHT = height;
+export const SCREEN_WIDTH = width;
